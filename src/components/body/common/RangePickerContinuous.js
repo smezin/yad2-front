6e52ec5cab64c33ffef53e2f31d6facd2ee54243
Shahaf Mezin <smezin@gmail.com>
2020-10-11 19:04:11 +0300

    checkpoint

--- src/components/body/common/RangePickerContinuous.js
+++ src/components/body/common/RangePickerContinuous.js
@@ -11,18 +11,6 @@ const RangePickerContinuous = (props) => {
     const [minDisplay, setMinDisplay] = useState(addSeperator(filters.search[minFilter]) || minPlaceHolder)
     const [maxDisplay, setMaxDisplay] = useState(addSeperator(filters.search[maxFilter]) || maxPlaceHolder)
 
-    const onBlur = (e, set) => {
-        if (e.target.value === '') {
-            switch(set) {
-                case 'min':
-                    return setMinDisplay(minPlaceHolder)
-                case 'max':
-                    return setMaxDisplay(maxPlaceHolder)
-                default: 
-                    return
-            } 
-        }
-    }
     const handleChange = (e, set) => {
         let isNumber = undefined
         if (isNumeric(removeSeperator(e.target.value))) {
@@ -70,19 +58,7 @@ const RangePickerContinuous = (props) => {
                 break
         }
     }
-    const clearPlaceHolder = (e, set) => {
-        if (e.target.value === minPlaceHolder || e.target.value === maxPlaceHolder) {
-            e.target.value = ''
-            switch(set) {
-                case 'min':
-                    return setMinDisplay('')
-                case 'max':
-                    return setMaxDisplay('')
-                default:
-                    return
-            }
-        }
-    }
+
     useEffect( () => {        
         isNumeric(removeSeperator(minDisplay)) ? dispatch(setMin(parseFloat(removeSeperator(minDisplay)))) 
         : dispatch(setMin(undefined))
@@ -92,17 +68,19 @@ const RangePickerContinuous = (props) => {
         isNumeric(removeSeperator(maxDisplay)) ? dispatch(setMax(parseFloat(removeSeperator(maxDisplay)))) 
         : dispatch(setMax(undefined))
     },[maxDisplay, setMax, dispatch])
-    
+    const style = {
+        color: 'black'
+    }
     return (
         <div className="range-picker">
             <div className="range-picker__header">
               {headerLocalName}
             </div>
             <div className="range-picker__input-container">
-                <input className={`range-picker__min ${minDisplay !== minPlaceHolder ? 'has-data':''}`} type="text" value={minDisplay} 
-                onChange={(e) => handleChange(e, 'min')} onFocus={(e) => clearPlaceHolder(e, 'min')} onBlur={(e) => onBlur(e, 'min')} />
-                <input className={`range-picker__max ${maxDisplay !== maxPlaceHolder ? 'has-data':''}`} type="text" value={maxDisplay} 
-                onChange={(e) => handleChange(e, 'max')} onFocus={(e) => clearPlaceHolder(e, 'max')} onBlur={(e) => onBlur(e, 'max')} />
+                <input style={style} className="range-picker__min" type="text" onChange={(e) => handleChange(e, 'min')}
+                placeholder={minPlaceHolder} />
+                <input className="range-picker__max" type="text" onChange={(e) => handleChange(e, 'max')} 
+                placeholder={maxPlaceHolder} />
             </div>
         </div>
     )
--- src/components/body/realestate/searchBar/advancedSearch/EntryDate.js
+++ src/components/body/realestate/searchBar/advancedSearch/EntryDate.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { setMinEntryDate } from 'actions/filters'
+import { setMinEntryDate, incAdvancedFilters } from 'actions/filters'
 import fetchFromResource from 'utility/fetchFromResource'
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
@@ -7,24 +7,24 @@ import he from 'date-fns/locale/he'
 import { FiltersContext } from 'context/FiltersContext'
 
 const EntryDate = () => {
-    const { dispatch } = useContext(FiltersContext)
+    const { dispatch, filters } = useContext(FiltersContext)
     const headerLocalName = fetchFromResource('string', 'advancedSearch', 'date', 'localName')
     const localPlaceHolder = fetchFromResource('string', 'advancedSearch', 'date', 'localPlaceHolder')
-    const [entryDate, setEntryDate] = useState(false)
+    const [entryDate, setEntryDate] = useState(filters.search.minEntryDate || false)
     
     const handleEntryPick = (pickedDate) => {
+        !entryDate && dispatch(incAdvancedFilters())
         setEntryDate(pickedDate)
         dispatch(setMinEntryDate(pickedDate))
     }
-   
     
     return(
         <div className="entry-date">
             <div className="entry-date__header">
                 {headerLocalName}
             </div>
-            <DatePicker className="entry-date__input" maxDate={entryDate} 
-            selected={entryDate} onChange={date => handleEntryPick(date)} locale={he} />
+            <DatePicker className="entry-date__input" minDate={new Date()} 
+            selected={entryDate} onChange={date => handleEntryPick(date)} locale={he} placeholderText={localPlaceHolder} />
         </div>
     )
 }
