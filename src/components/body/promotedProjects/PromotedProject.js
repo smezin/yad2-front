514d0147f61826c31b0f1081e53900572ce7cdd4
Shahaf Mezin <smezin@gmail.com>
2020-11-12 09:59:50 +0200

    ribbon now has color option as prop, and two deifferent alternating colors as default

--- src/components/body/promotedProjects/PromotedProject.js
+++ src/components/body/promotedProjects/PromotedProject.js
@@ -3,7 +3,7 @@ import fetchFromResource from 'utility/fetchFromResource'
 import FavoriteHeart from '../realestate/feed/FavoriteHeart'
 
 const PromotedProject = (props) => {
-    const { item } = props
+    const { item, color } = props
     if (!item || !item.location || !item.text) {
         return null
     }
@@ -12,14 +12,26 @@ const PromotedProject = (props) => {
     const imgUrl = item.imageUrls[0]
     const text = item.text
     const ribbonLocalName = item.ribbonText || fetchFromResource('string', 'promotedProjects', 'defaultRibbonText', 'localName')
-    
+    const themeMainColor = '#ff7100'
+    const ribbonAltColor = '#0495ff'
+    const ribbonColors = [themeMainColor, ribbonAltColor]
+    let ribbonColor = themeMainColor
+    if (color) {
+        ribbonColor = color === 'alt' ? ribbonAltColor : color
+    } else {
+        ribbonColor = ribbonColors[Math.floor(Math.random() * ribbonColors.length)]
+    }
+    const style = {
+        background: ribbonColor,
+        borderColor: ribbonColor
+    }
     return (
         <div className="promoted-project">
             <div className="promoted-project__image-container">
                 <img src={imgUrl} alt="project-pic"/>
                 <FavoriteHeart item={item} />
             </div>           
-            <div className="ribbon">{ribbonLocalName}</div>
+            <div className="ribbon" style={style}>{ribbonLocalName}</div>
             <div className="promoted-project__description">
                 <div className="promoted-project__address">{street}</div> 
                 <div className="promoted-project__city">{city}</div> 
--- src/components/body/promotedProjects/PromotedProjectsBar.js
+++ src/components/body/promotedProjects/PromotedProjectsBar.js
@@ -21,11 +21,11 @@ const PromotedProjectsBar = () => {
     mutableItemsList = mutableItemsList.filter((item) => item._id.localeCompare(promotedItem2._id))
     const promotedItem3 = mutableItemsList.length > 0 ? 
     mutableItemsList[Math.floor(Math.random() * mutableItemsList.length)] : undefined
-    return ( 
+    return ( //can add 'color' prop to set ribbons color. 'main' and 'alt' are preset colors"
         <div className="promoted-projects-bar">
-            <PromotedProject item={promotedItem1} />
-            <PromotedProject item={promotedItem2} />
-            <PromotedProject item={promotedItem3} />
+            <PromotedProject item={promotedItem1} color="main" />
+            <PromotedProject item={promotedItem2} color="main" />
+            <PromotedProject item={promotedItem3} color="alt" />
         </div>
     )
 }
